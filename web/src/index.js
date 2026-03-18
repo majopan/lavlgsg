3db18f4551b01a5daeca2f6ee71145aa48bd121e
Maria Jose Jimenez Diaz <mariazzz2326@gmail.com>
2026-03-17 23:45:21 -0500

    Update index.js

--- web/src/index.js
+++ web/src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+// Importar reportWebVitals (viene con CRA)
+import reportWebVitals from './reportWebVitals';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+// Si quieres medir el rendimiento
 reportWebVitals();
